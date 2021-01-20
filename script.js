a6cbb529842c72fb541492adf2892ae7e8d51087
ernie <ernieperez96@gmail.com>
2021-01-19 17:04:55 -0800

    added script js and search func

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#search-button").on("click", function() {
+        var searchValue = $("#search-value").val();
+    
+        // clear input box
+        $("#search-value").val("");
+    
+        searchWeather(searchValue);
+      });
+
+      
+});
