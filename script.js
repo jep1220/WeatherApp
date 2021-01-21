5932a0450f67745321d68e09738a0903dc7986ae
ernie <ernieperez96@gmail.com>
2021-01-20 17:28:36 -0800

    additional html cards using ajax

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ $(document).ready(function() {
               type: "GET", 
               url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=e6cf8c17c234148e2aa482d264e65514",
               dataType: "json",
-              success: function(data) {
-                if (history.indexOf(searchValue) === -1) {
+              success:function(data) {
+                if(history.indexOf(searchValue) === -1) {
                   history.push(searchValue);
                   window.localStorage.setItem("history", JSON.stringify(history));            
                   makeRow(searchValue);
@@ -34,7 +34,11 @@ $(document).ready(function() {
 
                   var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
                   var card = $("<div>").addClass("card");
-                                     
+                  var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+                  var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+                  var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
+                  var cardBody = $("<div>").addClass("card-body");
+                  var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
               }
           })
