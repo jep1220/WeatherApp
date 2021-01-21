60ede0138f6bdabc4f8aa1d3223225ca023402f8
ernie <ernieperez96@gmail.com>
2021-01-20 16:36:50 -0800

    added ajax search weather

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ $(document).ready(function() {
       });
 
 
+      function makeRow(text) {
+        var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+        $(".history").append(li);
+      }
+
+      function searchWeather(searchValue) {
+          $.ajax({
+              type: "GET", 
+              url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=e6cf8c17c234148e2aa482d264e65514",
+              dataType: "json",
+              success: function(data) {
+                  if (history.indexOf(searchValue) === -1) {
+                      history.push(searchValue);
+                      window.localStorage.setItem("history", JSON.stringify(history));
+                      makeRow(searchValue);
+
+
+                  }
+              }
+          })
+      }
+
 });
