0673c0d83de198360a72fd5fe7b7ce595f89c811
ernie <ernieperez96@gmail.com>
2021-01-20 17:00:46 -0800

    added html via ajax

--- script.js
+++ script.js
@@ -24,13 +24,18 @@ $(document).ready(function() {
               url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=e6cf8c17c234148e2aa482d264e65514",
               dataType: "json",
               success: function(data) {
-                  if (history.indexOf(searchValue) === -1) {
-                      history.push(searchValue);
-                      window.localStorage.setItem("history", JSON.stringify(history));
-                      makeRow(searchValue);
+                if (history.indexOf(searchValue) === -1) {
+                  history.push(searchValue);
+                  window.localStorage.setItem("history", JSON.stringify(history));            
+                  makeRow(searchValue);
+                }
 
+                  $("today").empty();
 
-                  }
+                  var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+                  var card = $("<div>").addClass("card");
+                                     
+
               }
           })
       }
